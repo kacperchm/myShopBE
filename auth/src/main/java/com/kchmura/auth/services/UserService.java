dfff72c89ba746ae0926d0d5bceb83e01b1183f7
kacperchm <chmuraka1998@gmail.com>
2024-05-15 21:51:10 +0200

    Create method to add users in UserService

---
+++ auth/src/main/java/com/kchmura/auth/services/UserService.java
@@ -0,0 +1,21 @@
+package com.kchmura.auth.services;
+
+import com.kchmura.auth.entity.User;
+import com.kchmura.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.saveAndFlush(user);
+    }
+
+}
